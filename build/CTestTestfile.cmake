2dc2718d9b993aeb3b52133b45190aae9a6e13b3
jay <smyshlenyj@gmail.com>
2023-08-10 22:10:43 +0300

    - build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jay/Skillfactory/chat_gui
-# Build directory: /home/jay/Skillfactory/chat_gui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/glfw-build")
--- build/_deps/glfw-build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jay/Skillfactory/chat_gui/build/_deps/glfw-src/src
-# Build directory: /home/jay/Skillfactory/chat_gui/build/_deps/glfw-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
